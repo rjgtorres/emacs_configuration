9bd849445d18bfbfddd0152a70fc3621b3292e93
Ricardo Torres <ricardojgtorres@gmail.com>
2020-09-13 19:54:43 +0100

    first commit

---
+++ init.el
@@ -0,0 +1,42 @@
+
+
+;;;-----------------------------------------------------------------------------
+;;; Modifications mail (ver sc-mail.el)
+;;;-----------------------------------------------------------------------------
+(setq user-full-name "Ricardo Torres")
+(setq user-mail-address "rtorres@siscog.pt")
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/git/orgfiles/todo.org")))
+ '(package-selected-packages
+   (quote
+    (helm-spotify-plus helm use-package autopair writeroom-mode engine-mode all-the-icons org-superstar centaur-tabs rainbow-delimiters powerline auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+  ;; Set repositories
+  (require 'package)
+  (setq-default
+   load-prefer-newer t
+   package-enable-at-startup nil)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)
+
+  ;; Install dependencies
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package t))
+  (setq-default
+   use-package-always-defer t)
+(require 'org)
+(org-babel-load-file "~/.emacs.d/torresconfig.org")
---
+++ search_engines.el
@@ -0,0 +1,66 @@
+;; this file contains search engine defenitions for engine-mode
+
+(defengine amazon
+  "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+
+(defengine amauk
+  "http://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+
+(defengine amade
+  "http://www.amazon.de/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+
+(defengine amaes
+  "http://www.amazon.es/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+
+(defengine duckduckgo
+  "https://duckduckgo.com/?q=%s"
+  :keybinding "d")
+
+(defengine github
+  "https://github.com/search?ref=simplesearch&q=%s")
+
+(defengine google
+  "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
+  :keybinding "g")
+
+(defengine google-images
+  "http://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
+
+(defengine google-maps
+  "http://maps.google.com/maps?q=%s"
+  :docstring "Mappin' it up.")
+
+;; (defengine project-gutenberg
+;;   "http://www.gutenberg.org/ebooks/search/?query=%s")
+
+;; (defengine qwant
+;;   "https://www.qwant.com/?q=%s")
+
+;; (defengine rfcs
+;;   "http://pretty-rfc.herokuapp.com/search?q=%s")
+
+(defengine stack-overflow
+  "https://stackoverflow.com/search?q=%s")
+
+(defengine twitter
+  "https://twitter.com/search?q=%s")
+
+(defengine wikipedia
+  "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+  :keybinding "w"
+  :docstring "Searchin' the wikis.")
+
+(defengine wiktionary
+  "https://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
+
+(defengine wolfram-alpha
+  "http://www.wolframalpha.com/input/?i=%s")
+
+(defengine youtube
+  "http://www.youtube.com/results?aq=f&oq=&search_query=%s"
+  :keybinding "y")
+
+(defengine hyperspec
+  "http://www.lispworks.com/cgi-bin/search.cgi?q=%s&t=-D--HB-"
+  :keybinding "l")
+
