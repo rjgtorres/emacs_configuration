2941de6aabbeb63e4a308a7bce9a78edea2e4603
RTorres <rjtorres@criticalsoftware.com>
2026-04-24 17:19:17 +0100

    Add openstreet maps support

--- extras/rdt-misc.el
+++ extras/rdt-misc.el
@@ -30,8 +30,32 @@
     (writeroom-restore-window-config t)
     :hook
     (writeroom-mode-hook . my/writeroom_configs))
+
   (use-package vterm
     :ensure t)
   )
+
+(use-package osm
+  :ensure t
+  :bind ("C-c m" . osm-prefix-map) ;; Alternatives: `osm-home' or `osm'
+
+  :custom
+  ;; Take a look at the customization group `osm' for more options.
+  (osm-default-server 'default) ;; Configure the tile server
+  (osm-default-zoom 15)         ;; Default zoom level
+  (osm-copyright nil)             ;; Display the copyright information
+  (osm-home (list calendar-latitude calendar-longitude 10))       ;; Home, configure `calendar-latitude/longitude' instead
+  :config
+  ;; (osm-add-server 'cycletravel
+  ;;   :name "Cycle.travel"
+  ;;   :description "The best cycling planing app"
+  ;;   :url
+  ;; Add custom servers, see also https://github.com/minad/osm/wiki
+  ;; (osm-add-server 'myserver
+  ;;   :name "My tile server"
+  ;;   :group "Custom"
+  ;;   :description "Tiles based on aerial images"
+  ;;   :url "https://myserver/tiles/%z/%x/%y.png?apikey=%k")
+)
 
 (provide 'rdt-misc)
